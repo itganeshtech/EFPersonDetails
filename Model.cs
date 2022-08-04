9bf6f6ac428b278a13d09344a9f912b8145c786b
itganeshtech <it.ganesh.tech@gmail.com>
2022-08-04 20:28:39 +0530

    Add project files.

---
+++ Model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFPersonDetails
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> Persons { get;set; }
+        public DbSet<Address> Addresss { get; set; }
+
+        public string DbPath { get; }
+        public PersonContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "person.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+    }
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public int SocialSecurityNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+    }
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
